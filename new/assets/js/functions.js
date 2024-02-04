233109488a0adb27075a5e4a9c74c40b4bcb7c2f
nerotvlive <daniel.niesmann@outlook.com>
2024-02-04 15:06:47 +0100

    Updated new page beta

--- new/assets/js/functions.js
+++ new/assets/js/functions.js
@@ -1,8 +1,24 @@
-function checkDevice() {
-    const isMobile = /iPhone|iPad|iPod|Android|Windows Phone/i.test(navigator.userAgent);
-    if (isMobile) {
-        document.getElementById("head-menu").classList.remove("active");
+window.addEventListener('resize', checkWindowSize);
+
+function checkWindowSize() {
+    if(window.innerWidth > 600) {
+        activateMenu()
     } else {
-        document.getElementById("head-menu").classList.add("active");
+        deactivateMenu();
     }
+}
+
+function activateMenu() {
+    const isMobile = /iPhone|iPad|iPod|Android|Windows Phone/i.test(navigator.userAgent);
+    if (!isMobile) {
+        document.getElementById("head-menu").classList.add("active");
+        document.getElementById("sub-menu").classList.add("active");
+        document.getElementById("content").classList.add("active");
+    }
+}
+
+function deactivateMenu() {
+    document.getElementById("head-menu").classList.remove("active");
+    document.getElementById("sub-menu").classList.remove("active");
+    document.getElementById("content").classList.remove("active");
 }
